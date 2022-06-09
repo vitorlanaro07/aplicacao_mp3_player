95a7704e15d14207c78cf902f46af756b193ac4a
Vitor S. Lanaro <vitor.lanaro@hotmail.com>
2022-06-09 16:57:31 -0300

    Subindo versão 1.0

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-import os
-import time
+
 
 import pygame as pg
+
+
 from modulos import botao, musicas
+
+pg.font.init()
+font = pg.font.SysFont("urwbookman", 20)
 
 
 def main():
-    janela.fill(color[0])
-    pg.draw.rect(janela, color[1], desenho)
+    janela.fill(color)
     iniciar_mixer()
     musica = "musicas/sound.mp3"
-    musica_inicializou = False
+    img = font.render("3030 - Mundo de Ilusões", True, (0, 0, 0))
+    janela.blit(img, (70, 20))
+
     janela.blit(imagem_capa_musica, (30, 50))
     # aplicacao
     rodando = True
-    pausou = False
+    iniciou = False
 
     while rodando:
         pos = pg.mixer.music.get_pos() / 1000
         print("Tempo:", pos)
-        if not pausou:
+
+        if not iniciou:
             if botao_start.draw(janela):
                 inicializar(musica)
+                iniciou = True
+        else:
+            if botao_start.draw(janela):
+                despausar()
         if botao_pause.draw(janela):
             pausar()
-            pausou = True
-            print("Pause")
-
-
-
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -40,8 +45,7 @@ def main():
 pg.init()
 
 #constantes
-desenho =((30,50),(335,375))
-color = ((90, 178, 219), (90, 213, 219))
+color = (90, 178, 219)
 
 #janela
 janela_largura = 400
@@ -51,15 +55,11 @@ pg.display.set_caption("Mp3 Player")
 
 #botoes
 imagem_botao_start = pg.image.load("imagens/botao-play.png").convert_alpha()
-imagem_botao_avancar = pg.image.load("imagens/avanco-rapido.png").convert_alpha()
-imagem_botao_retroceder = pg.image.load("imagens/voltar.png").convert_alpha()
 imagem_botao_pausa = pg.image.load("imagens/botao-de-pausa.png").convert_alpha()
 imagem_capa_musica = pg.image.load("imagens/capa.png").convert_alpha()
 
-botao_retroceder = botao.Botao(imagem_botao_retroceder, 0.55, 0, 470)
-botao_avancar = botao.Botao(imagem_botao_avancar, 0.55, 100, 470)
-botao_start = botao.Botao(imagem_botao_start, 0.55, 200, 470)
-botao_pause = botao.Botao(imagem_botao_pausa,0.55, 300, 470)
+botao_start = botao.Botao(imagem_botao_start, 0.55, 100, 470)
+botao_pause = botao.Botao(imagem_botao_pausa,0.55, 230, 470)
 
 
 
@@ -70,9 +70,11 @@ def inicializar (musica):
     pos = pg.mixer.music.get_pos()/1000
     print(pos)
 
-def pausar ():
+def pausar():
     pg.mixer.music.pause()
 
+def despausar():
+    pg.mixer.music.unpause()
 
 def get_mixer_args():
     pg.mixer.init()
